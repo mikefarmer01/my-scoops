83b689404195a6f34cd4d75666f4c87a2defbbea
Michael Bauer <mikefarmer01@protonmail.ch>
2022-08-13 19:25:17 +0200

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "mikefarmer01/my-scoops:main"
+    [String]$upstream = "mikefarmer01/my-scoops:master"
 )
 
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
